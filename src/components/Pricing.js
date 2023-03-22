99b1c0554be31e407d51a29266570c29595d74c4
Valeryia Beizer <strelcova.lera94@gmail.com>
2023-03-22 13:17:50 +0100

    updated swedish phrases

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -7,26 +7,22 @@ function Pricing() {
     return (
       <section id="pricing">
         <div>
-          <h2>{t('prices')}</h2>
-          <p className="about-section">
-            {t('validation')} "Vår-2023".
-          </p>
-          <p className="about-section">
-            {t('price_info')}
-          </p>
+          <h2>{t("prices")}</h2>
+          <p className="about-section">{t("validation")} "Vår-2023".</p>
+          <p className="about-section">{t("price_info")}</p>
           <div className="row row-pricing text-center">
             <div className="col-12 col-sm-12 col-md-4 col-lg-4 cards-pr">
               <div className="card">
-                <h3>{t('beginners')}</h3>
+                <h3>{t("beginners")}</h3>
                 <ul>
                   <li>
-                    {t('ones')}:{" "}
+                    {t("one_time")}:{" "}
                     <h4>
                       1600<small className="text-muted fw-light"> sek</small>
                     </h4>
                   </li>
                   <li>
-                    {t('twice')}:{" "}
+                    {t("twice")}:{" "}
                     <h4>
                       2500<small className="text-muted fw-light"> sek</small>
                     </h4>
@@ -36,22 +32,22 @@ function Pricing() {
             </div>
             <div className="col-12 col-sm-12 col-md-4 col-lg-4 cards-pr">
               <div className="card">
-                <h3>{t('intermediate')}</h3>
+                <h3>{t("intermediate")}</h3>
                 <ul>
                   <li>
-                    {t('once')}:{" "}
+                    {t("one_time")}:{" "}
                     <h4>
                       1800<small className="text-muted fw-light"> sek</small>
                     </h4>
                   </li>
                   <li>
-                    {t('twice')}:{" "}
+                    {t("twice")}:{" "}
                     <h4>
                       2700<small className="text-muted fw-light"> sek</small>
                     </h4>
                   </li>
                   <li>
-                    {t('three')}:{" "}
+                    {t("three")}:{" "}
                     <h4>
                       3500<small className="text-muted fw-light"> sek</small>*
                     </h4>
@@ -64,19 +60,19 @@ function Pricing() {
                 <h3>Stretching</h3>
                 <ul>
                   <li>
-                    1 {t('class')}:{" "}
+                    1 {t("class")}:{" "}
                     <h4>
                       150<small className="text-muted fw-light"> sek</small>
                     </h4>
                   </li>
                   <li>
-                    {t('once')}:{" "}
+                    {t("once")}:{" "}
                     <h4>
                       1800<small className="text-muted fw-light"> sek</small>
                     </h4>
                   </li>
                   <li>
-                    {t('twice')}:{" "}
+                    {t("twice")}:{" "}
                     <h4>
                       2500<small className="text-muted fw-light"> sek</small>
                     </h4>
