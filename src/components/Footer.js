1c27fecb74b9d19881f47239d6f3efcebf57a92d
Valeryia Beizer <strelcova.lera94@gmail.com>
2024-01-04 21:04:36 +0100

    fixed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,13 @@
 import React from "react";
-import { AnimatedOnScroll } from "react-animated-css-onscroll";
 
 function Footer() {
   const year = new Date().getFullYear();
 
   return (
     <section id="footer">
-      <AnimatedOnScroll
-        animationIn="fadeInUp"
-        style={{
-          animationDuration: "5000ms",
-        }}
-      >
         <footer>
           ©<span>{year}</span> | Designed & built with 🖤 by Valeryia B.
         </footer>
-      </AnimatedOnScroll>
     </section>
   );
 }
