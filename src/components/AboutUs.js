40e3a21be8c5cc5d8d09d3797b92673d918f3087
Valeryia Beizer <strelcova.lera94@gmail.com>
2024-08-13 20:26:27 +0800

    Added gallery to About  section

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,9 +1,80 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { AnimatedOnScroll } from "react-animated-css-onscroll";
+import Slider from "react-slick";
 
 function AboutUs() {
   const { t } = useTranslation();
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  const gallery = [
+    {
+      src: "img1.png",
+      alt: "img1",
+    },
+    {
+      src: "img2.png",
+      alt: "img2",
+    },
+    {
+      src: "img3.png",
+      alt: "img3",
+    },
+    {
+      src: "img4.png",
+      alt: "img4",
+    },
+    {
+      src: "img5.png",
+      alt: "img5",
+    },
+    {
+      src: "img6.png",
+      alt: "img6",
+    },
+    {
+      src: "img7.png",
+      alt: "img7",
+    },
+    {
+      src: "img8.png",
+      alt: "img8",
+    }
+  ];
 
   return (
     <section id="about">
@@ -25,6 +96,15 @@ function AboutUs() {
             </span>{" "}
             {t("statue")}
           </p>
+          <div className="slider-container">
+            <Slider {...settings}>
+              {gallery.map(({ src, alt }, index) => (
+                <div key={index}>
+                  <img className="img-slider" src={src} alt={alt} />
+                </div>
+              ))}
+            </Slider>
+          </div>
         </AnimatedOnScroll>
       </div>
     </section>
